d498c5f9156803db95814564fff50c6e50412113
Aurasj <thelimeyt14@gmail.com>
2023-10-26 13:55:44 +0300

    lab2
    lab2

---
+++ lab2/src/Professor.java
@@ -0,0 +1,21 @@
+public class Professor {
+
+    String firstName;
+    String lastName;
+
+    @Override
+    public String toString() {
+        return "Professor{" + "firstName=" + firstName + ", lastName=" +
+                lastName + '}';
+    }
+
+    public Professor(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+    public String getFullname()
+    {
+        return firstName + " " + lastName;
+    }
+}
